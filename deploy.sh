500dd3ea5fc4e5977e2c0ffb07db2f5e4a410fc8
elaineyeh <10456018@ntub.edu.tw>
2021-02-13 00:09:55 +0800

    feat: git init

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+echo $CIRCLE_PROJECT_REPONAME
+cd $CIRCLE_PROJECT_REPONAME
+pwd
+git pull
+
+mkdir -p ../docker-compose
+envsubst < docker-compose-template.yml > docker-compose-$CIRCLE_PROJECT_REPONAME.yml
+cp docker-compose-$CIRCLE_PROJECT_REPONAME.yml ../docker-compose
+
+cd ../docker-compose
+pwd
+FILENAME=`ls ./*.yml`
+FILE="docker-compose"
+for EACHFILE in $FILENAME
+do
+   FILE+=" -f $EACHFILE"
+done
+FILE+=" up -d"
+echo $FILE
+echo $FILE > run.sh
+sh run.sh
